6b69c701060ed319b81008aac81ad690f8ed9532
Michael Andrew <michael@hazardmedia.co.nz>
2013-10-12 17:19:58 +1300

    Initial commit of project structure.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+__author__ = 'Michael'
+
+#imports - note the from syntax allows us to not have to use the fully qualified name each time we reference the class
+#imports follow the convention import package.subpackage.module import ClassName
+from nz.co.hazardmedia.sgdialer.controllers.AppController import AppController
+
+if __name__ == "__main__":
+
+    #print AppController
+    theApp = AppController()
+    theApp.on_execute()
---
+++ src/setup.py
@@ -0,0 +1,15 @@
+__author__ = 'Michael'
+
+from distutils.core import setup
+setup(name='controllers',
+      version='1.0',
+      packages=['controllers','co.hazardmedia.sgdialer.controllers'],
+      )
+setup(name='models',
+      version='1.0',
+      packages=['models','co.hazardmedia.sgdialer.models'],
+      )
+setup(name='views',
+      version='1.0',
+      packages=['views','co.hazardmedia.sgdialer.views'],
+      )
